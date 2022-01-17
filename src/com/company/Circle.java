6ebb62d74ad0ac9c584003b1b56f46f263ba8005
Elvira <akzholtursunov01@gmail.com>
2022-01-17 21:58:08 +0300

    Initial commit

---
+++ src/com/company/Circle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public  class Circle {
+    public static final double PI = 3.14;
+    public  static  void  area (double radius,double PI) {
+        System.out.println(PI * (radius * radius));
+    }
+    public static void circumferen (double radious, double PI) {
+        System.out.println(PI * 2 * radious );
+
+    }
+
+
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Circle.area(4,Circle.PI);
+        Circle.circumferen(3,Circle.PI);
+
+    }
+}
